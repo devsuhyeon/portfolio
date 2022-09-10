98c6408044a5c75f8d9b6c28eedcb079002f81f0
Suhyeon Lee <suhyeonlee.dev@gmail.com>
2022-09-10 22:59:56 +0800

    feat: add arrow up button

--- main.js
+++ main.js
@@ -3,6 +3,27 @@
 // Handle click on contact me button on the home section
 const contactMeBtn = document.querySelector('.home__contact');
 contactMeBtn.addEventListener('click', () => {
-  const scrollTo = document.querySelector('#contact');
-  scrollTo.scrollIntoView({ behavior: 'smooth' });
+  scrollIntoView('#contact');
 });
+
+// Show arrow up button when scrolling down
+const home = document.querySelector('#home');
+const homeHeight = home.getBoundingClientRect().height;
+const arrowUpBtn = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUpBtn.classList.add('visible');
+  } else {
+    arrowUpBtn.classList.remove('visible');
+  }
+});
+
+// Handle click on arrow up button
+arrowUpBtn.addEventListener('click', () => {
+  scrollIntoView('#home');
+});
+
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
