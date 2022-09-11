cd3db42f8ffea96e0099527655dee7c7787732a0
Suhyeon Lee <suhyeonlee.dev@gmail.com>
2022-09-11 15:08:45 +0800

    feat: handle navbar menu click + navbar toggle button

--- main.js
+++ main.js
@@ -1,5 +1,33 @@
 'use strict';
 
+// Make navbar transparent when it is on the top of the page
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark');
+  } else {
+    navbar.classList.remove('navbar--dark');
+  }
+});
+
+// Handle click on navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const link = event.target.dataset.link;
+  if (!link) {
+    return;
+  }
+  navbarMenu.classList.remove('open');
+  scrollIntoView(link);
+});
+
+// Handle navbar toggle button
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
+});
+
 // Handle click on contact me button on the home section
 const contactMeBtn = document.querySelector('.home__contact');
 contactMeBtn.addEventListener('click', () => {
